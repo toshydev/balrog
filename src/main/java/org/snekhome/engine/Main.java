3de7f1c371906c13080572cd6aa180e78b7bc253
Anton Roters <antonroters@hotmail.com>
2023-06-07 19:17:30 +0200

    add main class

---
+++ src/main/java/org/snekhome/engine/Main.java
@@ -0,0 +1,27 @@
+package org.snekhome.engine;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Main extends JFrame implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        Paint game = new Paint();
+    }
+}
